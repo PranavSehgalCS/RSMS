a0fef808a4f8240c668007a1b22429e02abe9a8a
ps1958 <ps1958@g.rit.edu>
2023-06-02 16:15:36 +0530

    BE: Ca & Co DoneD FE: Dashboard Done, Ca & Co Init

--- ERetail_UI/angular-workspace/src/app/app-routing.module.ts
+++ ERetail_UI/angular-workspace/src/app/app-routing.module.ts
@@ -9,6 +9,15 @@ import { AccPageComponent } from './components/Account_Components/acc-page/acc-p
 
 import { DashboardComponent } from './components/Navigation_Components/dashboard/dashboard.component';
 
+import { ViewCompanyComponent } from './components/Company_Component/view-company/view-company.component';
+import { EditCompanyComponent } from './components/Company_Component/edit-company/edit-company.component';
+import { CreateCompanyComponent } from './components/Company_Component/create-company/create-company.component';
+
+import { ViewCategoryComponent } from './components/Category_Components/view-category/view-category.component';
+import { CreateCategoryComponent } from './components/Category_Components/create-category/create-category.component';
+import { EditCategoryComponent } from './components/Category_Components/edit-category/edit-category.component';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -18,6 +27,16 @@ const routes: Routes = [
   { path: 'accountpage', component:AccPageComponent},
   
   { path: 'dashboard', component: DashboardComponent},
+
+  { path: 'companies', redirectTo: '/companies/view', pathMatch: 'full' }, 
+  { path:'companies/view', component: ViewCompanyComponent,pathMatch: 'prefix'},
+  { path:'companies/edit', component: EditCompanyComponent,pathMatch: 'prefix'},
+  { path:'companies/create', component: CreateCompanyComponent,pathMatch: 'prefix'},
+  
+  { path: 'categories', redirectTo: '/companies/view', pathMatch: 'full' }, 
+  { path: 'categories/view', component: ViewCategoryComponent, pathMatch: 'prefix'},
+  { path: 'categories/edit', component: EditCategoryComponent, pathMatch: 'prefix'},
+  { path: 'categories/create', component: CreateCategoryComponent, pathMatch: 'prefix'}
 ];
 
 @NgModule({
--- ERetail_UI/angular-workspace/src/app/app.module.ts
+++ ERetail_UI/angular-workspace/src/app/app.module.ts
@@ -14,12 +14,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { NavbarComponent } from './components/Navigation_Components/navbar/navbar.component';
 import { DashboardComponent } from './components/Navigation_Components/dashboard/dashboard.component';
 
+import { ViewCategoryComponent } from './components/Category_Components/view-category/view-category.component';
+import { EditCategoryComponent } from './components/Category_Components/edit-category/edit-category.component';
+import { CreateCategoryComponent } from './components/Category_Components/create-category/create-category.component';
+
+import { ViewCompanyComponent } from './components/Company_Component/view-company/view-company.component';
+import { EditCompanyComponent } from './components/Company_Component/edit-company/edit-company.component';
+import { CreateCompanyComponent } from './components/Company_Component/create-company/create-company.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    DashboardComponent
+    DashboardComponent,
+    ViewCompanyComponent,
+    ViewCategoryComponent,
+    CreateCategoryComponent,
+    EditCategoryComponent,
+    EditCompanyComponent,
+    CreateCompanyComponent
     ],
   imports: [
     FormsModule,
